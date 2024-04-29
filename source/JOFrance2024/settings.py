a766d05ff17baed33161b015f4eba76b31715bfa
Cheick <che_tangara@hotmail.fr>
2024-04-29 19:32:36 +0200

    setting.py modified static root added

--- source/JOFrance2024/settings.py
+++ source/JOFrance2024/settings.py
@@ -130,9 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR.joinpath('static'),
-]
+STATIC_ROOT = BASE_DIR.joinpath('static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR.joinpath('mediafiles')
 
